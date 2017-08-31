a87db9d9984216e92a31270470767418792e360a
carey <wutkechuang@163.com>
2017-08-31 22:55:47 +0800

    环境搭好

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'eval-source-map',
+    entry: __dirname + '/src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'app.js'
+    },
+    devserver: {
+        contentBase: './dist/',
+        historyApiFallback: true,
+        hot: true,
+        inline: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(png|jpg|JPG|gif|woff|woff2|eot|ttf|svg)$/,
+                laoder: 'url-loader?limit=8192'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html'
+        })
+    ]
+}
